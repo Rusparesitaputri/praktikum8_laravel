1bb70c46786f5003756fe9883e86b22f63311d51
rusparesitaputri <resitaputriruspa@gmail.com>
2023-05-22 19:48:27 +0700

    ini praktikkum8

---
+++ resources/views/greeting.blade.php
@@ -0,0 +1,22 @@
+@php
+    $arraymobil = ['suv', 'hatchbag', 'sedan', 'MPV'];
+@endphp
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    @forelse ($arraymobil as $mobil)
+        <h2>{{ $mobil }}</h2>
+    @empty
+        <h2>data kosong cuy</h2>
+    @endforelse
+</body>
+
+</html>
